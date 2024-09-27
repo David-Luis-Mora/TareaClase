423396b040b2ee06f5c399f5fb09432e3f5f5972
David-Luis-Mora <davidluismora@gmail.com>
2024-09-27 19:40:13 +0200

    Archivo del spotify

---
+++ DOR/JavaSpotify/src/js/index.js
@@ -0,0 +1,21 @@
+import songs from '../assets/songs/*.mp3';
+
+Object.keys(songs);
+
+
+
+
+const map = {};
+
+
+
+let aux = 1;
+
+for(var key of Object.keys(songs)){
+    // console.log( Object.keys(songs));
+    map[`item-${aux}`] = `${songs[key]}`;
+    aux++;
+
+}
+
+// console.log(map)
