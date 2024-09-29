5bb1726cd09864d88c95c3846cf84d09cfee61b6
23112001Davidluismora <davidluismora@gmail.com>
2024-09-29 18:47:48 +0100

    Cambio y entrega de Tarea

--- DPL/Conexion/conexion.php
+++ DPL/Conexion/conexion.php
@@ -1,6 +1,6 @@
 <?php
 
-$conn = mysqli_connect('localhost','root','v[ptz6XOM[6l(7G9','prueba');
-
+$conn = mysqli_connect('localhost','root','','prueba');
+//v[ptz6XOM[6l(7G9
 echo '<pre>';
 print_r($conn);
