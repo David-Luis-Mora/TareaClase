8c8c5204e7155b0cfb9e4112eaf83fb6f1495193
David-Luis-Mora <davidluismora@gmail.com>
2024-10-02 16:10:49 +0200

    Index modificado

--- DOR/JavaSpotify/src/js/Player.js
+++ DOR/JavaSpotify/src/js/Player.js
@@ -3,13 +3,14 @@ import Song, {play_song} from './Song.js'
 export default class Player{
     constructor(map){
         // Object.entries(map);
-        // let aux = 1;
+        let aux = 1;
         for(var[key,value] of Object.entries(map)){
             const s_key = key;
             const s_value = value;
-            const song = new Song(s_key,s_value);
+            const s_conver = `.cv${aux}`
+            const song = new Song(s_key,s_value,s_conver);
             play_song(song);
-            // aux++;
+            aux++;
         }
     }
 }
