11f0045832febe0240dcbc6f27bc20b9efd7574d
David-Luis-Mora <davidluismora@gmail.com>
2024-10-03 17:15:03 +0200

    Formulario y php

--- DPL/Formulario/recibir.php
+++ DPL/Formulario/recibir.php
@@ -1,7 +1,16 @@
 <?php
+// print_r($_POST);
 
-echo "Bienvenido";
+if(!$_POST){
+    header('Location: http://localhost/test/Clase/TareaClase/DPL/Formulario/');
 
+}
+
+$nombre = $_POST['nombre'];
+$sexo = $_POST['sexo'];
+
+echo "Hola $nombre es de sexo: $sexo";
+
+// echo "Bienvenido";
 
 ?>
-C:\Users\David\Desktop\Tarea\TareaClase\DPL\Formulario
