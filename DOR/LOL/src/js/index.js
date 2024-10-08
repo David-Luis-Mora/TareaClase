7fd9df63d7194a05102a57c6c4bf1308346b4de5
David-Luis-Mora <davidluismora@gmail.com>
2024-10-08 16:22:00 +0100

    Modificacion de LOL

--- DOR/LOL/src/js/index.js
+++ DOR/LOL/src/js/index.js
@@ -8,18 +8,15 @@ let page_count = 1
 function loadpage(estate){
     console.log(estate);
     if (estate == 0){
-        
+        console.log("Load incial")
         const page = document.getElementById("mostrar_imagenes");
-
         let count = 0;
         for(let i = 0; i <lista_champ.length; i++){
             if( count == 8 ){
                 break;
-
             }else{
                 count +=1
             }
-
             // console.log(lista_champ[i].tags.length)
 
             let type_1 = "";
@@ -47,7 +44,7 @@ function loadpage(estate){
                 <img src="${imageUrl}" alt="${lista_champ[i].name}">
                 <br>
                 <div class="types">
-                Type: ${tip_1}  ${tip_2} 
+                Type: ${type_1}  ${type_2} 
                 </div>
                 </div>`;
 
@@ -69,6 +66,19 @@ function loadpage(estate){
 
         for(let i = 0; i <lista_champ.length; i++){
                 if((page_count*8)-8<= i && i <(page_count*8)){
+                    let type_1 = "";
+                    let type_2 = "";
+
+                    if(lista_champ[i].tags.length == 2){
+                        type_1 = lista_champ[i].tags[0]
+                        type_2 = lista_champ[i].tags[1]
+                        // console.log(tip_1);
+                        // console.log(tip_2);
+
+                    }else{
+                        type_1 = lista_champ[i].tags[0]
+                        // console.log(tip_1);
+                    }
 
                     const champCard = document.createElement("div");
                     champCard.classList.add("champ");
@@ -79,9 +89,8 @@ function loadpage(estate){
                             <p>${lista_champ[i].name}</p>
                         <img src="${imageUrl}" alt="${lista_champ[i].name}"
                         <br>
-                        <p>Historia: ${lista_champ[i].history}.</p>
                         <div class="types">
-                        Tipo: ${lista_champ[i].tags}
+                            Type: ${type_1}  ${type_2} 
                         </div>`;
                     page.appendChild(champCard);
                 }
@@ -161,8 +170,7 @@ const obtenerChamp = async () => {
         });
         // console.log(lista_champ)
         cantidad = Math.ceil(lista_champ.length / 8);
-
-
+        console.log("Pagina Inical")
         loadpage(0)
 }
 
@@ -170,92 +178,9 @@ const obtenerChamp = async () => {
 obtenerChamp()
 
 
-// Comprobar si funciona
-// function botonDerecho(){
-//     console.log(cantidad)
-//     console.log(page_count)
-//     page_count += 1
-//     if(page_count > cantidad){
-//         return true;
-//     }
-//     console.log("boton derecho")
-//     const page = document.getElementById("mostrar_imagenes");
-//     page.innerHTML=""
-
-//     // console.log(lista_champ.length);
-
-//     for(let i = 0; i <lista_champ.length; i++){
-//             if((page_count*8)-8<= i && i <(page_count*8)){
-
-//                 const champCard = document.createElement("div");
-//                 champCard.classList.add("champ");
-//                 //   pokemonCard.id = `pokemon-${pokemon.id}`;
-//                 const imageUrl = `https://ddragon.leagueoflegends.com/cdn/13.18.1/img/champion/${lista_champ[i].image.full}`;
-//                 champCard.innerHTML = `
-//                     <div class="cardChamp">
-//                         <p>${lista_champ[i].name}</p>
-//                     <img src="${imageUrl}" alt="${lista_champ[i].name}"
-//                     <br>
-//                     <p>Historia: ${lista_champ[i].history}.</p>
-//                     <div class="types">
-//                     Tipo: ${lista_champ[i].tags}
-//                     </div>`;
-//                 page.appendChild(champCard);
-//             }
-
-
-//         }
-
-// }
-
-
-
-// function botonIzquierda(){
-//     console.log("boton izquierdo")
-//     // console.log(cantidad)
-    
-//     if(page_count <= 1){
-//         console.log(page_count)
-//         return true;
-//     }
-//     page_count -= 1
-//     console.log(page_count)
-
-
-//     const page = document.getElementById("mostrar_imagenes");
-//     page.innerHTML=""
-
-//     // console.log(lista_champ.length);
-
-//     for(let i = 0; i < lista_champ.length; i++){
-//             if((page_count*8)-8<= i && i <(page_count*8)){
-
-//                 const champCard = document.createElement("div");
-//                 champCard.classList.add("champ");
-//                 //   pokemonCard.id = `pokemon-${pokemon.id}`;
-//                 const imageUrl = `https://ddragon.leagueoflegends.com/cdn/13.18.1/img/champion/${lista_champ[i].image.full}`;
-//                 champCard.innerHTML = `
-//                     <div class="cardChamp">
-//                         <p>${lista_champ[i].name}</p>
-//                     <img src="${imageUrl}" alt="${lista_champ[i].name}"
-//                     <br>
-//                     <p>Historia: ${lista_champ[i].history}.</p>
-//                     <div class="types">
-//                     Tipo: ${lista_champ[i].tags}
-//                     </div>`;
-//                 page.appendChild(champCard);
-//             }
-
-
-//         }
-
-
-// }
-
-
 const btonDerech = document.getElementById("btonDerech");
 btonDerech.addEventListener("click", function(){
-    loadpage(-1)
+    loadpage(1)
 })
 
 
