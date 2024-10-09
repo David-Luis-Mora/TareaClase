3ca2395584e646ee1b96a332c82dada334039bf8
David-Luis-Mora <davidluismora@gmail.com>
2024-10-09 20:29:41 +0100

    ya me lleva a la pagina

--- DWS/supertodo/tasks/urls.py
+++ DWS/supertodo/tasks/urls.py
@@ -6,6 +6,6 @@ from . import views
 app_name = 'tasks'
 
 urlpatterns = [
-    path('', views.home, name='task'),
+    path('', views.home, name='home'),
     path('task/<task_slug>/', views.task_detail, name='task_detail'),
 ]
