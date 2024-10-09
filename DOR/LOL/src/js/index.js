26fbfabc1b228ab1719a0dd502633e987838d5d8
23112001Davidluismora <davidluismora@gmail.com>
2024-10-09 12:10:43 +0100

    Se muestra las skin en lo del LOL

--- DOR/LOL/src/js/index.js
+++ DOR/LOL/src/js/index.js
@@ -1,13 +1,123 @@
 import Campeones from './Campeones.js';
 
+let lista_champ = [];
+let cantidad = 0;
+let page_count = 1;
 
-let lista_champ = []
-let cantidad = 0
-let page_count = 1
+function showPopup(name, imageUrl, tags, infor_general) {
+    console.log("Pop-up")
+    const popup = document.getElementById("popup");
+    console.log(imageUrl)
+    popup.innerHTML = `
+        <div class="popup-content">
+            <h2>${name}</h2>
+            <button id="botnIzPop"><</button>
+            <div>
+                <img src="${imageUrl}" alt="${name}">
+            </div>
+            
+            <button id="botnDrPop">></button>
+                ${infor_general}
+            <p>Typs: ${tags}</p>
+            <button id="closePopup">Cerrar</button>
+        </div>`;
+        
+    popup.style.display = "flex";
+
+    document.getElementById("closePopup").addEventListener("click", function() {
+        popup.style.display = "none";
+    });
+
+    document.getElementById("botnDrPop").addEventListener("click", function(){
+
+        const popupImage = document.querySelector('.popup-content img');
+        let imageUrl = popupImage.getAttribute('src');
+
+        const regex = /_(\d+)\.jpg$/;
+        const match = imageUrl.match(regex);
+    
+        if (match) {
+            let currentNumber = parseInt(match[1], 10);
+            console.log("Número actual:", currentNumber);
+    
+            let newNumber = currentNumber + 1;
+            console.log("Nuevo número:", newNumber);
+    
+            let newImageUrl = imageUrl.replace(regex, `_${newNumber}.jpg`);
+
+            let img = new Image();
+            img.src = newImageUrl;
+
+            img.onload = function() {
+
+                popupImage.setAttribute('src', newImageUrl);
+                console.log("Imagen actualizada: ", newImageUrl);
+            };
+            
+            img.onerror = function() {
+
+                console.log("Imagen no encontrada:", newImageUrl);
+                alert("Imagen no encontrada, manteniendo la imagen actual.");
+            };
+
+
+        } else {
+            console.log("No se encontró un número en la URL.");
+        }
+
+
+
+    })
+    document.getElementById("botnIzPop").addEventListener("click", function(){
+
+        const popupImage = document.querySelector('.popup-content img');
+        let imageUrl = popupImage.getAttribute('src');
+
+        const regex = /_(\d+)\.jpg$/;
+        const match = imageUrl.match(regex);
+    
+        if (match) {
+            let currentNumber = parseInt(match[1], 10);
+            console.log("Número actual:", currentNumber);
+
+            let newNumber = currentNumber - 1;
+            console.log("Nuevo número:", newNumber);
+
+            let newImageUrl = imageUrl.replace(regex, `_${newNumber}.jpg`);
+
+            let img = new Image();
+            img.src = newImageUrl;
+
+            img.onload = function() {
+
+                popupImage.setAttribute('src', newImageUrl);
+                console.log("Imagen actualizada: ", newImageUrl);
+            };
+            
+            img.onerror = function() {
+
+                console.log("Imagen no encontrada:", newImageUrl);
+                alert("Imagen no encontrada, manteniendo la imagen actual.");
+            };
+            
+
+        } else {
+            console.log("No se encontró un número en la URL.");
+        }
+
+
+
+
+    });
+};
+
+
+
+
 
 function loadpage(estate){
-    console.log(estate);
     if (estate == 0){
+
         console.log("Load incial")
         const page = document.getElementById("mostrar_imagenes");
         let count = 0;
@@ -17,137 +127,114 @@ function loadpage(estate){
             }else{
                 count +=1
             }
-            // console.log(lista_champ[i].tags.length)
-
-            let type_1 = "";
-            let type_2 = "";
-            
-            if(lista_champ[i].tags.length == 2){
-                type_1 = lista_champ[i].tags[0]
-                type_2 = lista_champ[i].tags[1]
-                // console.log(tip_1);
-                // console.log(tip_2);
-
-            }else{
-                type_1 = lista_champ[i].tags[0]
-                // console.log(tip_1);
-            }
-
-            
             const champCard = document.createElement("div");
             champCard.classList.add("champ");
-            //   pokemonCard.id = `pokemon-${pokemon.id}`;
+
             const imageUrl = `https://ddragon.leagueoflegends.com/cdn/13.18.1/img/champion/${lista_champ[i].image.full}`;
             champCard.innerHTML = `
                 <div class="cardChamp">
                     <p>${lista_champ[i].name}</p>
-                <img src="${imageUrl}" alt="${lista_champ[i].name}">
+                <div>
+                    <img src="${imageUrl}" alt="${lista_champ[i].name}">
+                </div>
                 <br>
                 <div class="types">
-                Type: ${type_1}  ${type_2} 
+                    <p>Typs: ${lista_champ[i].tags}</p>
                 </div>
                 </div>`;
+            
+            let imag_full = `https://ddragon.leagueoflegends.com/cdn/img/champion/splash/${lista_champ[i].name}_0.jpg`
+            // console.log(imag_full)
+
+            
+
+            
+
+
+
+            champCard.addEventListener("click", function() {
+                showPopup(lista_champ[i].name, imag_full, lista_champ[i].tags, lista_champ[i].history);
+            });
+
 
             page.appendChild(champCard);
-
         }
-        
 
     } else if(estate == 1){
-        page_count += 1
-        if(page_count > cantidad){
+        if(page_count >= cantidad){
             return true;
         }
-        // console.log("boton derecho")
+        page_count += 1
         const page = document.getElementById("mostrar_imagenes");
         page.innerHTML=""
 
-        // console.log(lista_champ.length);
-
         for(let i = 0; i <lista_champ.length; i++){
-                if((page_count*8)-8<= i && i <(page_count*8)){
-                    let type_1 = "";
-                    let type_2 = "";
+            if((page_count*8)-8<= i && i <(page_count*8)){
+                const champCard = document.createElement("div");
+                champCard.classList.add("champ");
+                const imageUrl = `https://ddragon.leagueoflegends.com/cdn/13.18.1/img/champion/${lista_champ[i].image.full}`;
+                champCard.innerHTML = `
+                    <div class="cardChamp">
+                        <p>${lista_champ[i].name}</p>
+                    <img src="${imageUrl}" alt="${lista_champ[i].name}"
+                    <br>
+                    <div class="types">
+                        Typs: ${lista_champ[i].tags}
+                    </div>`;
 
-                    if(lista_champ[i].tags.length == 2){
-                        type_1 = lista_champ[i].tags[0]
-                        type_2 = lista_champ[i].tags[1]
-                        // console.log(tip_1);
-                        // console.log(tip_2);
+                // champCard.addEventListener("click", function() {
+                //     showPopup(lista_champ[i].name, imageUrl, lista_champ[i].tags);
+                // });
 
-                    }else{
-                        type_1 = lista_champ[i].tags[0]
-                        // console.log(tip_1);
-                    }
+                let imag_full = `https://ddragon.leagueoflegends.com/cdn/img/champion/splash/${lista_champ[i].name}_0.jpg`
+                console.log(imag_full)
 
-                    const champCard = document.createElement("div");
-                    champCard.classList.add("champ");
-                    //   pokemonCard.id = `pokemon-${pokemon.id}`;
-                    const imageUrl = `https://ddragon.leagueoflegends.com/cdn/13.18.1/img/champion/${lista_champ[i].image.full}`;
-                    champCard.innerHTML = `
-                        <div class="cardChamp">
-                            <p>${lista_champ[i].name}</p>
-                        <img src="${imageUrl}" alt="${lista_champ[i].name}"
-                        <br>
-                        <div class="types">
-                            Type: ${type_1}  ${type_2} 
-                        </div>`;
-                    page.appendChild(champCard);
-                }
-
-
-            }
-       
-
-        
-
+                champCard.addEventListener("click", function() {
+                    showPopup(lista_champ[i].name, imag_full, lista_champ[i].tags, lista_champ[i].history);
+                });
+                    
+                page.appendChild(champCard);
+            };
+        };
     }else{
         if(page_count <= 1){
-            // console.log(page_count)
             return true;
         }
         page_count -= 1
-        // console.log(page_count)
-    
-    
+        console.log(page_count)
         const page = document.getElementById("mostrar_imagenes");
-        page.innerHTML=""
-    
-        // console.log(lista_champ.length);
-    
+        page.innerHTML=""    
         for(let i = 0; i < lista_champ.length; i++){
-                if((page_count*8)-8<= i && i <(page_count*8)){
-    
-                    const champCard = document.createElement("div");
-                    champCard.classList.add("champ");
-                    //   pokemonCard.id = `pokemon-${pokemon.id}`;
-                    const imageUrl = `https://ddragon.leagueoflegends.com/cdn/13.18.1/img/champion/${lista_champ[i].image.full}`;
-                    champCard.innerHTML = `
-                        <div class="cardChamp">
-                            <p>${lista_champ[i].name}</p>
-                        <img src="${imageUrl}" alt="${lista_champ[i].name}"
-                        <br>
-                        <p>Historia: ${lista_champ[i].history}.</p>
-                        <div class="types">
-                        Tipo: ${lista_champ[i].tags}
-                        </div>`;
-                    page.appendChild(champCard);
-                }
-    
-    
+            if((page_count*8)-8<= i && i <(page_count*8)){
+                const champCard = document.createElement("div");
+                champCard.classList.add("champ");
+                //   pokemonCard.id = `pokemon-${pokemon.id}`;
+                const imageUrl = `https://ddragon.leagueoflegends.com/cdn/13.18.1/img/champion/${lista_champ[i].image.full}`;
+                champCard.innerHTML = `
+                    <div class="cardChamp">
+                        <p>${lista_champ[i].name}</p>
+                    <img src="${imageUrl}" alt="${lista_champ[i].name}"
+                    <br>
+                    <div class="types">
+                    Typs: ${lista_champ[i].tags}
+                    </div>`;
+
+                // champCard.addEventListener("click", function() {
+                //     showPopup(lista_champ[i].name, imageUrl, lista_champ[i].tags);
+                // });
+
+                let imag_full = `https://ddragon.leagueoflegends.com/cdn/img/champion/splash/${lista_champ[i].name}_0.jpg`
+
+
+                champCard.addEventListener("click", function() {
+                    showPopup(lista_champ[i].name, imag_full, lista_champ[i].tags, lista_champ[i].history);
+                });
+
+                page.appendChild(champCard);
             }
-
+        }
     }
-
-
-    
-
-
 }
-
-
-
-
 
 
 const obtenerChamp = async () => {
@@ -156,24 +243,18 @@ const obtenerChamp = async () => {
             return result.json();
         })
         .then(function(result) {
-            const champ_list = result.data; // Accedemos a la clave "data" que contiene los campeones
-            // console.log(champ_list);
+            const champ_list = result.data;
 
-            // Recorremos el objeto champ_list
             for (let champ in champ_list) {
-                // console.log(champ); // Aquí tienes el nombre del campeón
-                // console.log(champ_list[champ]); // Aquí tienes los detalles del campeón
                 let nuevoChamp = new Campeones(champ_list[champ]);
                 lista_champ.push(nuevoChamp);
-                // console.log(nuevoChamp);
             }
         });
-        // console.log(lista_champ)
+
         cantidad = Math.ceil(lista_champ.length / 8);
         console.log("Pagina Inical")
         loadpage(0)
 }
-
 
 obtenerChamp()
 
@@ -189,6 +270,12 @@ btonIzquierd.addEventListener("click", function(){
     loadpage(-1)
 
 } )
+
+
+
+
+
+
 
 
 
