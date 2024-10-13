cd31b4f3041abb0148649eeb1f0f05ceb5956fbd
23112001Davidluismora <davidluismora@gmail.com>
2024-10-13 18:23:01 +0100

    Limpieza de codigo, comentario y de estructura de carpeta

--- DOR/LOL/src/js/Campeones.js
+++ DOR/LOL/src/js/Campeones.js
@@ -1,6 +1,4 @@
-// Exportamos por defecto la clase Pokemon
 export default class Campeones {
-    // Constructor que recibe como parámetro data que contiene los datos de los Pokemon que obtenemos desde la API
     constructor(data) {
         this.name = data.name;                          
         this.id = data.id;                              
--- DOR/LOL/src/js/index.js
+++ DOR/LOL/src/js/index.js
@@ -3,29 +3,23 @@ import Campeones from './Campeones.js';
 let lista_champ = [];
 let cantidad = 0;
 let page_count = 1;
-let skinIndex = 0; // Índice de skin
-let selectedChampionId = null; // ID del campeón seleccionado
+let skinIndex = 0;
 
-// Función para obtener los detalles de un campeón específico
+
 async function getChampionDetails(championId) {
     const url = `https://ddragon.leagueoflegends.com/cdn/13.18.1/data/es_ES/champion/${championId}.json`;
     const response = await fetch(url);
     const data = await response.json();
-    return data.data[championId]; // Devuelve solo los datos del campeón específico
+    return data.data[championId];
 }
 
-// Función para mostrar el popup
+
 function showPopup(name, imageUrl, tags, infor_general, id) {
-    // console.log(name, imageUrl, tags, infor_general, id)
     const page = document.getElementById("card-grid");
     const popup = document.getElementById("championModal");
     const previousContent = page.innerHTML;
-    // console.log("Primera vez que entra al pop")
-    // console.log(previousContent)
 
-    selectedChampionId = id; // Guardar el ID del campeón seleccionado
 
-    // Oculta los botones de navegación
     document.getElementById("btonIzquierd").style.visibility = "hidden";
     document.getElementById("btonDerech").style.visibility = "hidden";
 
@@ -45,26 +39,18 @@ function showPopup(name, imageUrl, tags, infor_general, id) {
 
     popup.classList.add("show");
 
-    // Evento de cerrar el popup
     document.getElementById("close").addEventListener("click", function() {
         popup.classList.remove("show");
-        // console.log("Primera vez que sale del pop")
-        // console.log(previousContent)
-        // Restaurar el contenido original del card-grid
         page.innerHTML = previousContent;
-        page.id = "card-grid"; // Restaurar el ID del contenedor
+        page.id = "card-grid";
         page.classList.remove("championModal");
 
-        // Restaurar la visibilidad de los botones de navegación
         document.getElementById("btonIzquierd").style.visibility = "visible";
         document.getElementById("btonDerech").style.visibility = "visible";
 
-        // Volver a asignar los eventos de las cartas después de cerrar el popup
         const all_cards = document.querySelectorAll('.card');
-        // console.log(all_cards.values())
         
         all_cards.forEach((card, index) => {
-            // console.log(card, index)
             let cambiado = true;
             card.addEventListener('click', function() {
                 console.log("Esto en el bucle")
@@ -89,34 +75,30 @@ function showPopup(name, imageUrl, tags, infor_general, id) {
         });
     });
 
-    // Cargar las skins del campeón
     getChampionDetails(id).then(championDetails => {
         const skins = championDetails.skins;
         const totalSkins = skins.length;
 
-        // Cambiar a la skin anterior
         document.getElementById("prev").addEventListener("click", function() {
-            skinIndex = (skinIndex - 1 + totalSkins) % totalSkins;  // Circular hacia atrás
+            skinIndex = (skinIndex - 1 + totalSkins) % totalSkins;
             const skin = skins[skinIndex];
             const newImageUrl = `https://ddragon.leagueoflegends.com/cdn/img/champion/splash/${id}_${skin.num}.jpg`;
             document.getElementById("champion-image").src = newImageUrl;
         });
 
-        // Cambiar a la siguiente skin
         document.getElementById("next").addEventListener("click", function() {
-            skinIndex = (skinIndex + 1) % totalSkins;  // Circular hacia adelante
+            skinIndex = (skinIndex + 1) % totalSkins;
             const skin = skins[skinIndex];
             const newImageUrl = `https://ddragon.leagueoflegends.com/cdn/img/champion/splash/${id}_${skin.num}.jpg`;
-            // console.log(newImageUrl)
             document.getElementById("champion-image").src = newImageUrl;
         });
     });
 }
 
-// Función para cargar la página con las tarjetas de campeones
+
 function loadpage() {
     const page = document.getElementById("card-grid");
-    page.innerHTML = ""; // Limpiar el contenido actual
+    page.innerHTML = "";
 
     let startIndex = (page_count - 1) * 8;
     let endIndex = Math.min(startIndex + 8, lista_champ.length);
@@ -136,7 +118,6 @@ function loadpage() {
             <p>${formato_text}</p>
         `;
 
-        // Evento de clic para abrir el popup
         champCard.addEventListener("click", function() {
             showPopup(lista_champ[i].name, imag_full, lista_champ[i].tags, lista_champ[i].history, lista_champ[i].id);
         });
@@ -145,7 +126,6 @@ function loadpage() {
     }
 }
 
-// Función para obtener la lista de campeones
 const obtenerChamp = async () => {
     await fetch("https://ddragon.leagueoflegends.com/cdn/13.18.1/data/es_ES/champion.json")
         .then(result => result.json())
@@ -158,12 +138,11 @@ const obtenerChamp = async () => {
         });
 
     cantidad = Math.ceil(lista_champ.length / 8);
-    loadpage(); // Cargar la primera página
+    loadpage();
 }
 
 obtenerChamp();
 
-// Botones de paginación
 document.getElementById("btonDerech").addEventListener("click", function() {
     if (page_count < cantidad) {
         page_count++;
