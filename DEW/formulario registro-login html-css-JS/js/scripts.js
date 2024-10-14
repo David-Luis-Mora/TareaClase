965dd46cdb691cc5ba11ec50f1824e1822e5883d
David-Luis-Mora <davidluismora@gmail.com>
2024-10-14 17:08:55 +0100

    Modificacion del index.js

--- DEW/formulario registro-login html-css-JS/js/scripts.js
+++ DEW/formulario registro-login html-css-JS/js/scripts.js
@@ -4,34 +4,34 @@
 // npm install -g live-server
 // live-server
 
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.13.2/firebase-app.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
- 
- 
- 
- // Import the functions you need from the SDKs you need
- import { initializeApp } from "https://www.gstatic.com/firebasejs/10.13.2/firebase-app.js";
- // TODO: Add SDKs for Firebase products that you want to use
- // https://firebase.google.com/docs/web/setup#available-libraries
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAV9d4KTlk3-75SZ7dWUvDj4InxZ_QuKCc",
+  authDomain: "compra-pokemon-86250.firebaseapp.com",
+  projectId: "compra-pokemon-86250",
+  storageBucket: "compra-pokemon-86250.appspot.com",
+  messagingSenderId: "887503594687",
+  appId: "1:887503594687:web:12672a053bf4d36e55d20a",
+};
 
- // Your web app's Firebase configuration
- const firebaseConfig = {
-   apiKey: "AIzaSyAV9d4KTlk3-75SZ7dWUvDj4InxZ_QuKCc",
-   authDomain: "compra-pokemon-86250.firebaseapp.com",
-   projectId: "compra-pokemon-86250",
-   storageBucket: "compra-pokemon-86250.appspot.com",
-   messagingSenderId: "887503594687",
-   appId: "1:887503594687:web:12672a053bf4d36e55d20a"
- };
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
 
- // Initialize Firebase
- const app = initializeApp(firebaseConfig);
-
-
-
-import { getFirestore, collection, doc, addDoc , getDoc, getDocs } from "https://www.gstatic.com/firebasejs/10.13.2/firebase-firestore.js";
+import {
+  getFirestore,
+  collection,
+  doc,
+  addDoc,
+  getDoc,
+  getDocs,
+  sett,
+} from "https://www.gstatic.com/firebasejs/10.13.2/firebase-firestore.js";
 const db = getFirestore(app); // Inicializa Firestore
-
-
 
 async function obtenerUsuario(id) {
   const docRef = doc(db, "Usuarios", id); // Reemplaza "0" con el ID correcto del documento
@@ -44,59 +44,25 @@ async function obtenerUsuario(id) {
   }
 }
 
-
-
 async function agregarUsuario(name, apellidos, edad, email, password) {
-  try {
-    const docRef = await addDoc(collection(db, "Usuarios",), {
-      nombre: name,
-      apellidos: apellidos,
-      edad: edad,
-      email: email,
-      password: password,
-    });
+  const data = {
+    nombre: name,
+    apellidos: apellidos,
+    edad: edad,
+    email: email,
+    password: password,
+  };
 
-    console.log("Documento agregado con ID: ", docRef.id);
-    console.log((name, apellidos, edad, email, password))
-  } catch (e) {
-    console.error("Error agregando documento: ", e);
-  }
+  await setDoc(doc(db, "Usuarios", `${email}`), data);
+  // try {
+  //   const docRef = await sett(doc(db, "Usuarios", `${email}`), {});
+
+  //   console.log("Documento agregado con ID: ", docRef.id);
+  //   console.log((name, apellidos, edad, email, password));
+  // } catch (e) {
+  //   console.error("Error agregando documento: ", e);
+  // }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 // Expresión regular para validar email
 const emailPattern = /.+@.+\..+/;
@@ -107,9 +73,9 @@ const passwordPattern =
   /^(?=.*[A-Za-z])(?=.*[A-Z])(?=.*\\d)(?=.*[!@#$%^&*])[A-Za-z\\d!@#$%^&*]{8,}$/;
 
 function validateLogin() {
-  prueba()
+  prueba();
   const username = document.getElementById("loginUsername").value;
-  const password = document.getElementById("loginPassword").value;  // Limito longitud del campo
+  const password = document.getElementById("loginPassword").value; // Limito longitud del campo
   if (username.length >= 10) {
     alert("El nombre de usuario debe tener al menos 3 caracteres.");
     return false;
@@ -123,15 +89,11 @@ function validateLogin() {
     return false;
   }
 
-  
-
-
-
   return true;
 }
 
 function validateRegister() {
-  console.log("Validar registro")
+  console.log("Validar registro");
   // const name = document.getElementById("name").value;
   // const apellidos = document.getElementById("fullName").value;
   // const email = document.getElementById("email").value;
@@ -176,15 +138,14 @@ function validateRegister() {
   //   alert("Las contraseñas no coinciden.");
   //   return false;
   // }
-  console.log("Agregaria el usuario")
-  agregarUsuario(name,apellidos,edad,email,password)
+  // console.log("Agregaria el usuario")
+  // agregarUsuario(name,apellidos,edad,email,password)
   // setTimeout(() => {
   //   // console.clear(); // Limpia la consola después de un segundo
   // }, 1000000000);
 
   return true;
 }
-
 
 async function obtenerDocumento() {
   const docRef = doc(db, "lista", "0"); // Reemplaza "0" con el ID correcto del documento
@@ -197,9 +158,6 @@ async function obtenerDocumento() {
   }
 }
 
-
-
-
 function prueba() {
   console.log("Verificacion de registro");
   const registerUsername = document.getElementById("registerUsername").value; // Este es el campo que buscas
@@ -209,15 +167,11 @@ function prueba() {
   const password = document.getElementById("registerPassword").value;
   const confirmPassword = document.getElementById("confirmPassword").value;
 
-  console.log("Valores de entrada:",typeof( registerUsername, apellidos, edad, email, password, confirmPassword));
-  agregarUsuario(registerUsername,apellidos,edad,email,password);
+  if (obtenerUsuario())
+    agregarUsuario(registerUsername, apellidos, edad, email, password);
 }
 
-
-
-
-
-function login(){
+function login() {
   // if(validateLogin()){
   //   // obtenerDocumento();
   //   console.log("Login hecho")
@@ -225,48 +179,27 @@ function login(){
   // }else{
   //   alert("El usuario no existe")
   // }
-  console.log("Login hecho")
-  if(validateLogin){
-    console.log("Login aceptado")
-    let a = "b"
-    obtenerUsuario(a)
-
-
-
+  console.log("Login hecho");
+  if (validateLogin) {
+    console.log("Login aceptado");
+    let a = "b";
+    obtenerUsuario(a);
   }
-
-  
-
-
 }
 
-function register(){
+function register() {
   event.preventDefault();
-  console.log("Registro hecho")
-  prueba()
+  console.log("Registro hecho");
+  prueba();
   // if(validateRegister){
   //   console.log("El registro es valido")
   //   agregarUsuario()
 
   // }
-
-
-
 }
 
+const v_login = document.getElementById("btonLogin");
+v_login.addEventListener("click", login);
 
-
-
-
-
-
-
-
-
-const v_login = document.getElementById("btonLogin")
-v_login.addEventListener("click",login)
-
-
-const v_register = document.getElementById("btn_register")
-v_register.addEventListener("click",register)
-
+const v_register = document.getElementById("btn_register");
+v_register.addEventListener("click", register);
